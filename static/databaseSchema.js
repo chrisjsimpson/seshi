a7563c13ff2cecc24ab5691e0040414dfd69809e
chris <enquiries@karmacomputing.co.uk>
2015-01-18 16:14:03 +0000

    Retrieving number of files for a given bod id complete.

--- static/databaseSchema.js
+++ static/databaseSchema.js
@@ -4,7 +4,7 @@ var db = new Dexie('flytipper');
 //Define a schema
 db.version(1)
     .stores({
-        chunks: '++id, fileId, boxId'
+        chunks: '[fileId+chunkNumber],*fileId, boxId'
     });
 
 //Open the database

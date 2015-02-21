ebc703cfc6467c00c70618384ae2e41262e57070
Chris <chris15leicester@gmail.com>
2015-02-21 05:08:32 +0000

    Create Locall SDP indecriminently of weather connected or not

---
+++ static/boxHandling.js
@@ -0,0 +1,142 @@
+window.onload = function () { 
+//Event listner to set Boxid
+  window.boxId = 'myBoxID'; //Default box id
+  var elm = document.getElementById("setBoxId");
+  elm.addEventListener("click", setBoxId, false);
+
+  function setBoxId(event)
+  {
+       var elm = document.getElementById("boxId");
+       window.boxId = elm.value;
+  }//End setBoxId
+
+  var showFiles = document.getElementById('showFiles');
+  showFiles.addEventListener("click", showBoxFiles, false);
+
+  function showBoxFiles(event)
+  {
+    console.log("Show files in box eventlistner called..."); 
+    var filesInBox = document.getElementById('filesInBox');
+    //Get list of files for given boxId.
+    files = {
+            fill:function(cb){
+                db.transaction("rw", db.chunks, function() {
+                var i = 0;
+                var sum = 0;
+                var count = 0;
+                var id = '';
+                db.chunks.where('boxId').equals(window.boxId).each(
+                    function(boxChunks) {
+                        //console.log(boxChunks.fileId);
+                       
+                        if(id != boxChunks.fileId)
+                        {
+                            files.chunks[i] = {fileId:boxChunks.fileId, fileName: boxChunks.fileName};            
+                            i++;
+                        }
+                        id = boxChunks.fileId;
+                        //console.log("Comparing: '" + files.chunks[sum].fileId + "' with: '" + boxChunks.fileId);
+                                //Check Array.observe exists
+                                if(typeof Array.observe != "undefined")
+                                {
+                                 Array.observe(files.chunks, trythis);
+                                }
+                        })//End get fileList 
+                        files.readyState = 'DONE';
+           }).then(function() {
+
+                         files.chunks.forEach(function(elm) {
+                                        //console.log(elm);
+                                    });
+                var fileNames = [];
+                fileNames = files.chunks;
+                console.log("There are currently " + fileNames.length + " files in boxId: " + window.boxId);
+
+                for(var i=0; i< fileNames.length;i++) {
+                    console.log(fileNames[i].fileId);
+                }
+
+                //Update files list box
+                var filesInBox = document.getElementById('filesInBox');
+                var list = '<ul>';
+                
+                for(var i=0;i<fileNames.length;i++)
+                {
+                        list += '<li title="Download"><a class="fileDownload" href="';
+                        list += 'index.html?download=';
+                        list += fileNames[i].fileId;
+                        list += '" data-fileId="';
+                        list += fileNames[i].fileId;
+                        list += '">';
+                        list += fileNames[i].fileName;
+                        list += '</a></li>';
+                }
+
+                list += '</ul>';
+                console.log(list);
+                filesInBox.innerHTML = list;
+
+                //Add event listeners to these files for downloading
+                var downloads = document.getElementsByClassName('fileDownload');
+                for(var i=0;i<downloads.length;i++)
+                {
+                    //Get fileId
+                    var fileId = downloads[i];
+                    //Add downloadFile event listener
+                    fileId.addEventListener('click', downloadFile, false);
+                }//End add download event listener to each file link.
+ 
+            }).catch(function(err) {
+                    console.log(err);
+                });
+            },
+            chunks:[],
+            readyState:''
+    }//End files object
+
+
+files.fill(trythis);
+
+  }//End showBoxFiles()
+
+function downloadFile(event) {
+    event.preventDefault();    
+    var fileId = event.target.getAttribute('data-fileid');
+
+    //Query IndexedDB to get the file
+    db.transaction('r', db.chunks, function() {
+        
+            db.chunks.where("fileId").equals(fileId).toArray(function(chunks) {
+                //Transaction scope
+                console.log("Found " + chunks.length + " chunks");
+                
+                var allChunksArray = [];
+                //Just get blob cunks
+                for(var i=0;i<chunks.length; i++){
+                    //console.log(found[i].chunk);
+                    allChunksArray[i] = chunks[i].chunk
+                }
+
+                var file = new Blob(allChunksArray, {type:chunks[0].fileType});
+                url = window.URL.createObjectURL(file);
+                console.log("Data: " + url);
+                //Open file
+                window.open(url);
+            })
+        
+        }).then(function() {
+            //Transaction completed
+
+        }).catch (function (err) {
+            
+            console.error(err);
+    
+    });//End get fildIdChunks from fileId
+
+}//End download file
+
+
+function trythis(updates) {
+    //console.log(updates[0].object.length);
+}
+};
--- static/clientXHRSignalingChannel.js
+++ static/clientXHRSignalingChannel.js
@@ -1,10 +1,3 @@
-// Copyright 2013-2014 Digital Codex LLC
-// You may use this code for your own education.  If you use it
-// largely intact, or develop something from it, don't claim
-// that your code came first.  You are using this code completely
-// at your own risk.  If you rely on it to work in any particular
-// way, you're an idiot and we won't be held responsible.
-
 // This code creates the client-side commands for an XML HTTP
 // Request-based signaling channel for WebRTC.
 

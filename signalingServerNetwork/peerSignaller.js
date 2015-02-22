68ddbef883422d299dfa78c2387aaa33155c7d3c
Chris <chris15leicester@gmail.com>
2015-02-22 22:43:13 +0000

    Peer signaller is now collecting SDP advertisements and storing them in peers object ready for contacting

--- signalingServerNetwork/peerSignaller.js
+++ signalingServerNetwork/peerSignaller.js
@@ -1,67 +1,173 @@
-connections = {}; /* Connections is an object with objects attatched named by their CIDs which contain their related BOXid and SDP data */
+var http = require("http");
+var url = require("url");
 
-var util = require('util'); //nodejs utilities
+// Creates a handler to collect POSTed data and to route the
+// request based on the path name
+function start(handle, port) {
+  function onRequest(req, res) {
+    var urldata = url.parse(req.url,true),
+        pathname = urldata.pathname,
+        info = {"res": res,
+                "query": urldata.query,
+                "postData":""};
+ 
+    //log("Request for " + pathname + " received");
+    req.setEncoding("utf8");
+    req.addListener("data", function(postDataChunk) {
+      info.postData += postDataChunk;
+      //log("Received POST data chunk '"+ postDataChunk + "'.");
+    });
+    req.addListener("end", function() {
+      route(handle, pathname, info);
+    });
+  }
 
-//Listen for a new SDP (Session description) offer or answer
-var express = require('express');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-var http = require('http');
-var query = require('url');
+  http.createServer(onRequest).listen(port);
+ 
+  console.log("Server started on port " + port);
+}
 
-var app = express();
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-	extended: false 
-}));
-app.use( bodyParser.json() );
-
-
-
-//http://tinyurl.com/m3wv7ym
-app.use(function(req, res) {
-	res.setHeader("Content-Type","text/plain");
-    	//res.setHeader("Access-Control-Allow-Origin", "*");
-	res.write("Test");
-	res.end(util.inspect(req));
-  });
-
-
-//app.post('/', jsonParser, function (req, res) {
-//	console.log("Saving message ***" + req.body);
-//	debugger;
-	//Add node to connections object of peers wanting to find each other
-		//Check dosn't already exist (peer has found their friend!)
-//		if ( typeof connections[req.body.cid] == "undefined" ) {
-//			console.log("First time connect, should add to connections object...");	
-//		} //End add first time connect peer to connections object
-
-	//messagesFor[partner[postData.id]].push(postData.message);
-	
-	//peers[req.body];
-//	var test = {"Hello":"Sucess... ;)"};
-//  	res.send(JSON.stringify(test));
-//})//End show posted data
+// Determines whether requested path is a static file or a custom
+// path with its own handler
+function route(handle, pathname, info) {
+      //console.log("About to route a request for " + pathname);
+      handleCustom(handle, pathname, info);
+}
 
 
-	//res.setHeader('Access-Control-Allow-Origin', '*');
-	//var boxId = query.parse(req.url, true).query.changeToBoxId;
+// Confirm handler for non-file path, then execute it
+function handleCustom(handle, pathname, info) {
+  if (typeof handle[pathname] == 'function') {
+    handle[pathname](info);
+  } else {
+    noHandlerErr(pathname, info.res);
+  }
+}
 
-	//Push data into peers array
-	//peers.push(boxId);
 
-	//console.log("boxId is: " + boxId);
-        //res.write('boxId is: ' + boxId + '\r\n' );
+// If no handler is defined for the request, return 404
+function noHandlerErr(pathname, res) {
+  console.log("No request handler found for " + pathname);
+  res.writeHead(404, {"Content-Type": "text/plain"});
+  res.write("404 Page Not Found");
+  res.end();
+}
 
-	//console.log(JSON.stringify(peers));
-	
-	//Print peer count
-	//console.log("There are: " + peers.length + ' peers');
 
-var server = app.listen(8000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+var connections = {},
+    partner = {},
+    messagesFor = {},
+    peers = {}; //Object of Peers waiting to be connected to indexed by their uuid. peer[uuid].sdp 
 
-	console.log("Listening at http://%s:%s", host, port);
-})
+
+// queue the sending of a json response
+function webrtcResponse(response, res) {
+  /* log("replying with webrtc response " +
+      JSON.stringify(response)); */
+  res.writeHead(200, {"Content-Type":"application/json"});
+
+  if(response.msgs != "" && typeof response.err != "string" && typeof response != "string")
+  {
+  	console.log("WebrtcResponse: \r\n" + JSON.stringify(response) + '\r\n');
+  }
+
+  res.write(JSON.stringify(response));
+  res.end();
+}
+
+
+// send an error as the json WebRTC response
+function webrtcError(err, res) {
+  //log("replying with webrtc error:  " + err);
+  webrtcResponse({"err": err}, res);
+}
+
+
+// Queues message in info.postData.message for sending to the
+// partner of the id in info.postData.id
+function send(info) {
+  console.log("postData received is ***" + info.postData + "***");
+  var postData = JSON.parse(info.postData),
+      res = info.res;
+
+  //Add partner to messagesFor object if not already apart of it
+  if ( typeof messagesFor[partner[postData.id]] == 'undefined' ) {
+	console.log("First time this partner has connected wooo!");
+        peers[postData.id] = [];
+	peers[postData.id].push(postData.message);
+  }
+
+  //messagesFor[partner[postData.id]].push(postData.message);
+	debugger;
+  if (typeof postData === "undefined") {
+    webrtcError("No posted data in JSON format!", res);
+    return;
+  }
+  if (typeof (postData.message) === "undefined") {
+    webrtcError("No message received", res);
+    return;
+  }
+  if (typeof (postData.id) === "undefined") {
+    webrtcError("No id received with message", res);
+    return;
+  }
+  if (typeof (partner[postData.id]) === "undefined") {
+    webrtcError("Invalid id " + postData.id, res);
+    return;
+  }
+  if (typeof (messagesFor[partner[postData.id]]) ===
+              "undefined") {
+    webrtcError("Invalid id " + postData.id, res);
+    return;
+  }
+  messagesFor[partner[postData.id]].push(postData.message);
+  console.log("Saving message ***" + postData.message +
+      "*** for delivery to id " + partner[postData.id]); 
+  webrtcResponse("Saving message ***" + postData.message +
+                 "*** for delivery to id " +
+                 partner[postData.id], res);
+}
+
+
+// Returns all messages queued for info.postData.id
+function get(info) {
+  var postData = JSON.parse(info.postData),
+      res = info.res;
+
+  if (typeof postData === "undefined") {
+    webrtcError("No posted data in JSON format!", res);
+    return;
+  }
+  if (typeof (postData.id) === "undefined") {
+    webrtcError("No id received on get", res);
+    return;
+  }
+  if (typeof (messagesFor[postData.id]) === "undefined") {
+    webrtcError("Invalid id " + postData.id, res);
+    return;
+  }
+
+  /* log("Sending messages ***" +
+      JSON.stringify(messagesFor[postData.id]) + "*** to id " +
+      postData.id); */
+  webrtcResponse({'msgs':messagesFor[postData.id]}, res);
+  messagesFor[postData.id] = [];
+}
+
+
+var port = process.argv[2] || 8000;
+
+
+// returns 404
+function fourohfour(info) {
+  var res = info.res;
+  //log("Request handler fourohfour was called.");
+  res.writeHead(404, {"Content-Type": "text/plain"});
+  res.write("404 Page Not Found");
+  res.end();
+}
+
+var handle = {};
+handle["/send"] = send;
+
+start(handle, port);

1f22791f17509c757c7d2c1a38a5bd41d02800f4
Chris <chris15leicester@gmail.com>
2015-03-04 14:25:36 +0000

    Switcing branches to fix blog support not implimented yet error in chrome

--- static/qrcode.js
+++ static/qrcode.js
@@ -363,6 +363,7 @@ var QRCode;
 			this._bIsPainted = false;
 			this._elImage = document.createElement("img");
 			this._elImage.alt = "Scan me!";
+			this._elImage.id = "QrImg";
 			this._elImage.style.display = "none";
 			this._el.appendChild(this._elImage);
 			this._bSupportDataURI = null;

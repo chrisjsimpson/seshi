5f694f8414c3bfc95aa3448cb1090b39bd168fb8
Christopher Simpson <chris15leicester@gmail.com>
2016-02-24 20:20:56 +0000

    Added smooth scroll to progress bar when storing a new file

--- js/SeshiSkin.example.js
+++ js/SeshiSkin.example.js
@@ -189,6 +189,8 @@ function refreshFileList() {
 
 function storeFile(fileList){
     Seshi.store({'dataSource':'fileSystem','data':fileList});
+    //Scroll to file list so user can see storing progress bar indicator:
+    smoothScroll('shareButtonsRow');
 }
 
 function deleteFile(event){
@@ -378,3 +380,48 @@ function updateStoreProgressDisplay() {
         localFileListBox.insertAdjacentHTML('afterend', output);
     }//End loop through each item in Seshi.storeProgress & update the display accordingly
 }//End updateStoreProgressDisplay()
+
+function smoothScroll(eID) {
+    function currentYPosition() {
+        // Firefox, Chrome, Opera, Safari
+        if (self.pageYOffset) return self.pageYOffset;
+        // Internet Explorer 6 - standards mode
+        if (document.documentElement && document.documentElement.scrollTop)
+            return document.documentElement.scrollTop;
+        // Internet Explorer 6, 7 and 8
+        if (document.body.scrollTop) return document.body.scrollTop;
+        return 0;
+    }
+
+    function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+    }
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 10);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+}
+

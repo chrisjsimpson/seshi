15b0591fb501bbd127f4abb72beb7c2c78830e02
Christopher Simpson <chris15leicester@gmail.com>
2016-02-25 17:02:52 +0000

    Deleted old displayFiles()

--- js/SeshiSkin.example.js
+++ js/SeshiSkin.example.js
@@ -118,46 +118,6 @@ function updateWhatsAppShareBtn() {
     whatsAppShareBtn[0].href = 'whatsapp://send?text=' + Seshi.getShareUrl();
 }//End updateWhatsAppShareBtn
 
-function displayFiles() {
-    fileList = Seshi.localFileList();
-    table = '<h2>Your files:</h2>\n<table>\n';
-    theader = '<thead>\n' +
-               '<tr class="header-row">\n' +
-                    '<th class="fileName" class="ascending">Name</th>\n' +
-                    '<th class="actions">Actions</th>\n' +
-               '</thead>\n';
-
-    tbodyStart = '<tbody>';
-    tbodyContent = '';
-
-        //Loop over local files list to create table body
-        for (var i=0; i< fileList.length;i++) {
-            tbodyContent += '<tr>\n' +
-                                '<td data-col="fileName" data-id="' + fileList[i].fileId + '"' +
-                                ' onclick="play(event)">' + fileList[i].fileName + '</td>\n' +
-                                '<td data-col="actions">' +
-                                    //Playback action
-                                    '<span data-id="' + fileList[i].fileId + '" onclick="play(event)">Play</span> / ' +
-                                    //Download action
-                                    '<span data-id="' + fileList[i].fileId + '" onclick="download(event)">Download</span> /' +
-                                    //Share action
-                                    '<span data-id="' + fileList[i].fileId + '" onclick="share(event)"> Send to peer</span> / ' +
-                                    //Play in sync action
-                                    '<span data-id="' + fileList[i].fileId + '" onclick="playInSync(event)"> Play in-sync with friend!</span> / ' +
-                                    //Delete action
-                                    '<span data-id="' + fileList[i].fileId + '" onclick="deleteFile(event)"> Delete file</span>' +
-                                    
-                                '</td>\n' +
-                           '</tr>\n\n';
-        }
-
-    tbodyEnd = '</tbody>';
-    tableOutput = table + theader + tbodyStart + tbodyContent + tbodyEnd + '</table>';
-    //Update filetable
-    var fileTable = document.getElementById('fileTable');
-    fileTable.innerHTML = tableOutput;
-}//End displayFiles
-
 
 function play(event) {
     console.log("My player implimentation...");

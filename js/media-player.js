29cbc2fbf448716bb513de74de9f8cd821f4718a
Anthony Ho <ahomnky11@gmail.com>
2016-02-26 13:06:27 +0000

    jquery if plyr has audio tag change css of entire .plyr

--- js/media-player.js
+++ js/media-player.js
@@ -30,6 +30,17 @@ $(".btn-hide").click(function() {
        }
 })
 
+//if audio player CSS change
+if($('.plyr').find('audio').length != 0) {
+    $('#hideall').css('position', 'relative');
+    $('.plyr').css({
+        'position': 'fixed',
+        'bottom': '0',
+        'width': '100%',
+        'z-index':'1001'
+});
+} 
+
 
 $(document).ready(function(){
 	//INITIALIZE

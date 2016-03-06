6bb5bc555f95b3e68f98481b24127b758931fea0
Anthony Ho <ahomnky11@gmail.com>
2016-03-06 17:30:31 +0000

    copyclipboard fadein delay3

--- ui/ui/main.js
+++ ui/ui/main.js
@@ -11830,7 +11830,7 @@ $("#copyclipboard").click(function () {
     });
     $(".keycopy .copied").replaceWith("<h6> Copied! </h6>")
 
-    $(".message-app-card").css('opacity', '1');
+    $(".message-app-card").delay(400).fadeIn();
 
 });
 

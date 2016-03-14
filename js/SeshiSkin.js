39d40a1df0ceed17ae9d51d856d680ec72c54a94
Anthony Ho <ahomnky11@gmail.com>
2016-03-14 23:05:37 +0000

    btn hide update4

--- js/SeshiSkin.js
+++ js/SeshiSkin.js
@@ -266,7 +266,8 @@ function play(event) {
                                   });
                           $("#hideall").css({'position':'absolute',
                                               'margin': '0 auto'});
-                          $("#hideall").addClass('widthOpenVideo');
+                          // $("#hideall").addClass('widthOpenVideo');
+                          $("addfilehide").hide();
                           $("#hideall").hide();
                             $('.btn-hide').show();
                         } else if ($(window).width() < 992 && $(window).width() > 768 ) {

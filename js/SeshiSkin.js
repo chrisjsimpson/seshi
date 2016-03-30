921ecf5f0e1fb02d163266cdc94ec5a720b5639c
Anthony Ho <ahomnky11@gmail.com>
2016-03-30 16:28:44 +0100

    alert box

--- js/SeshiSkin.js
+++ js/SeshiSkin.js
@@ -121,8 +121,49 @@ function peerConnectionBroken() {
     /* Called by event listener when sendFileProgressUpdate event is fired
      *  Used to display a break in Datachannel connection.
      * */
-    alert("Peer has disconnected");
-    alert("Hold on, we'll try and reconnect");
+
+
+     $(".dialog").before("<div class='dialogBlack'></div>");
+
+     $(".dialog").prepend('<div id="close">x</div>');
+
+    //  $("#dialogMe").click(function() {
+    //    dialogMe();
+    //  });
+
+     function dialogMe() {
+       $(".dialog").css("display", "block");
+       $(".dialogBlack").css("opacity", "0.2");
+       $(".dialogBlack").css("z-index", "250");
+       $(".dialog p, .dialog h2").css("opacity", "1");
+       $("#close").css("opacity", "1");
+       $("#okButton").css("opacity", "1");
+       $(".dialog").css("height", "185px");
+       $(".dialog").css("width", "300px");
+       $(".dialog").css("padding", "15px");
+       $(".dialog").css("margin-left", "-174.5px");
+       setTimeout(function() {
+         $(".dialog").css("opacity", "1");
+       }, 400);
+     }
+
+     $(".dialog #close, .dialog #okButton, .dialogBlack").click(function() {
+       $(".dialog p, .dialog h2").css("opacity", "0");
+       $(".dialogBlack").css("opacity", "0");
+       $(".dialogBlack").css("z-index", "-5");
+       $("#close").css("opacity", "0");
+       $("#okButton").css("opacity", "0");
+       $(".dialog").css("height", "0px");
+       $(".dialog").css("width", "0px");
+       $(".dialog").css("margin-left", "174.5px");
+       $(".dialog").css("padding", "0px");
+       setTimeout(function() {
+         $(".dialog").css("opacity", "0");
+       }, 400);
+     });
+
+    // alert("Peer has disconnected");
+    // alert("Hold on, we'll try and reconnect");
 
     $("#remoteFileListContainer").fadeOut();
     $("connectionStatus").hide();

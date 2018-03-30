f324030ccfac89166c52f5281f8257a6b5e9afbf
Christopher Simpson <hello@seshi.io>
2018-03-30 00:09:01 +0000

    clearing up submodule mess. re-introduced al files which has been put into seperate git modules

---
+++ Gulpfile.js
@@ -0,0 +1,122 @@
+/**
+ * Created by Meki on 2015.02.25..
+ */
+
+/* Get dependencies */
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifycss = require('gulp-cssnano'),
+    plumber = require( 'gulp-plumber' ),
+    jshint = require('gulp-jshint'),
+    livereload = require('gulp-livereload'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sassdoc = require('sassdoc'),
+    del = require('del');
+
+
+var onError = function( err ) {
+  console.log( 'An error occurred:', err.message );
+  this.emit( 'end' );
+}
+/* Set paths */
+
+var paths = {
+    /* Source paths */
+    styles: ['assets/sass/main.scss'],
+    scripts: [
+        'assets/bower_components/jquery/dist/jquery.js',
+        'assets/bower_components/jquery.easing/js/jquery.easing.js',
+        'assets/bower_components/bootstrap/dist/js/bootstrap.js',
+        'assets/js/ui.js',
+        'assets/js/responsive.js',
+        'assets/js/media-player.js'
+    ],
+    fonts: [
+        'assets/bower_components/bootstrap/fonts/*',
+        'assets/bower_components/font-awesome/fonts/*'
+    ],
+
+    /* Output paths */
+    stylesOutput: 'styles',
+    scriptsOutput: 'ui/ui',
+    imagesOutput: 'images',
+    fontsOutput: 'fonts'
+};
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+/* Tasks */
+
+gulp.task('styles', function () {
+  return gulp
+    .src(paths.styles)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(paths.stylesOutput))
+     .pipe(rename({suffix: '.min'}))
+     .pipe(minifycss())
+     .pipe(gulp.dest(paths.stylesOutput))
+     .pipe(notify({ message: 'Styles task complete' }));
+
+});
+
+gulp.task('styles', function () {
+  return gulp
+    .src(paths.styles)
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(gulp.dest(paths.stylesOutput));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(paths.scripts)
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest(paths.scriptsOutput))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.scriptsOutput))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src(paths.fonts)
+    .pipe(gulp.dest(paths.fontsOutput))
+    .pipe(notify({ message: 'Fonts task complete', onLast: true }));
+});
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch('assets/sass/*/**.scss', ['styles'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+
+
+gulp.task('clean', function(cb) {
+    del([paths.stylesOutput, paths.scriptsOutput, paths.imagesOutput, paths.fontsOutput], cb)
+});
+
+gulp.task('default', function() {
+    gulp.start('styles', 'scripts', 'fonts', 'watch');
+});
---
+++ databaseSchema.js
@@ -0,0 +1,27 @@
+//Make a new database conection
+var db = new Dexie('seshi');
+
+//Define a schema
+db.version(1)
+    .stores({
+        chunks: '[fileId+chunkNumber],fileId, boxId, chunkNumber'
+    });
+
+//Open the database
+db.open()
+    .catch(function(error){
+        alert('Oh oh : ' + error);
+    })
+
+//SeshiSignalSerer datbase
+var signalServerDb = new Dexie('seshiSignalServers');
+signalServerDb.version(1)
+	.stores({
+		signalServers: '&address,lastSuccessfulConnectTimestamp, lastConnectAttempTimestamp,numFailedConnectAttempts'
+	});
+//Open the signal servers database
+signalServerDb.open()
+	.catch(function(error){
+		alert('Oh no! : ' + error);
+	})
+
